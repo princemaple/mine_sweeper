f3b59ffdc6dae8a621ba5d4f26aecab01e3a5fd9
Po Chen <chenpaul914@gmail.com>
2022-01-09 16:09:36 +1100

    remove swoosh config

--- config/config.exs
+++ config/config.exs
@@ -25,16 +25,6 @@ config :tailwind,
     cd: Path.expand("../assets", __DIR__)
   ]
 
-# Configures the mailer
-#
-# By default it uses the "Local" adapter which stores the emails
-# locally. You can see the emails in your browser, at "/dev/mailbox".
-#
-# For production it's recommended to configure a different adapter
-# at the `config/runtime.exs`.
-config :mine_sweeper, MineSweeper.Mailer, adapter: Swoosh.Adapters.Local
-
-# Configure esbuild (the version is required)
 config :esbuild,
   version: "0.14.10",
   default: [
